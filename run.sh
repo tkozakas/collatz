8349f16b9fdf101a50187832b115cf028b43eb47
tomas6446 <tomas.kozakas@gmail.com>
2024-05-05 20:30:14 +0300

    Fix script

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ END=$2
 MODE=$3
 THREADS=$4
 
-javac -d build src/Main.java src/CollatzTask.java src/CollatzCalculator.java
-java -Xss5m -Xmx1g build/Main $START $END $MODE $THREADS
+javac -d build src/*.java
+java -Xss5m -Xmx1g -cp build Main $START $END $MODE $THREADS
